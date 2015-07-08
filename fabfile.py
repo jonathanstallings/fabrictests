35c2b76d5d1aa7dd5ddda3a2e3834175ca5a46c1
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-07 17:18:40 -0700

    Step 3 of tutorial

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,22 @@
 from fabric.api import run
 from fabric.api import env
+import boto.ec2
 
 env.hosts = ['localhost', ]
+env.aws_region = 'us-west-2'
+
 
 def host_type():
     run('uname -s')
+
+
+def get_ec2_connection():
+    if 'ec2' not in env:
+        conn = boto.ec2.connect_to_region(env.aws_region)
+        if conn is not None:
+            env.ec2 = conn
+            print "Connected to EC2 region %s" % env.aws_region
+        else:
+            msg = "Unable to connect to EC2 region %s"
+            raise IOError(msg % env.aws_region)
+    return env.ec2
