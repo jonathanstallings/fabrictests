bbdefafefaa9191570ab1d2a07857f5aaac3bb7b
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-07 17:27:17 -0700

    Step 4

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,8 @@
 from fabric.api import run
 from fabric.api import env
 import boto.ec2
+import time
+
 
 env.hosts = ['localhost', ]
 env.aws_region = 'us-west-2'
@@ -20,3 +22,35 @@ def get_ec2_connection():
             msg = "Unable to connect to EC2 region %s"
             raise IOError(msg % env.aws_region)
     return env.ec2
+
+
+def provision_instance(wait_for_running=False, timeout=60, interval=2):
+    wait_val = int(interval)
+    timeout_val = int(timeout)
+    conn = get_ec2_connection()
+    instance_type = 't2.micro'
+    key_name = 'pk-aws'
+    security_group = 'ssh-access'
+    image_id = 'ami-d0d8b8e0'
+
+    reservations = conn.run_instances(
+        image_id,
+        key_name=key_name,
+        instance_type=instance_type,
+        security_groups=[security_group, ]
+    )
+    new_instances = [i for i in reservations.instances if i.state == u'pending']
+    running_instance = []
+    if wait_for_running:
+        waited = 0
+        while new_instances and (waited < timeout_val):
+            time.sleep(wait_val)
+            waited += int(wait_val)
+            for instance in new_instances:
+                state = instance.state
+                print "Instance %s is %s" % (instance.id, state)
+                if state == "running":
+                    running_instance.append(
+                        new_instances.pop(new_instances.index(i))
+                    )
+                instance.update()
